c18ff5ce92343e1f0df4e54243151a816400816c
ChristopherP-C <christopherp.c.2day@gmail.com>
2024-12-09 09:54:51 -0600

    render job cards

--- client/src/pages/SearchJobs.tsx
+++ client/src/pages/SearchJobs.tsx
@@ -92,6 +92,7 @@ const SearchJobs = () => {
       //parse out data to match interface
 
       setSearchJobs(jobData);
+      console.log(`jobData:`, jobData)
 
     } catch (err) {
       console.error(err);
@@ -160,6 +161,28 @@ const SearchJobs = () => {
 
         <Row>
           {/* Render searched job results here */}
+          {searchJobs.map((job) => {
+            return (
+              <Col key={job.jobId} md={4}>
+                <Card className="custom-job-card">
+                  <Card.Body>
+                    <Card.Title>{job.jobTitle}</Card.Title>
+                    <Card.Text>{job.content}</Card.Text>
+                    <Card.Text>Company: {job.company.name}</Card.Text>
+                    <Card.Text>Location: {job.locations.map(location => location.name).join(', ')}</Card.Text>
+                    <Card.Text>Experience Level: {job.levels.map(level => level.name).join(', ')}</Card.Text>
+                    <Card.Text>Published: {job.datePublished}</Card.Text>
+                    <Button
+                      variant="primary"
+                      onClick={() => handleSaveJob(job.jobId)}
+                    >
+                      Save Job
+                    </Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+            )
+          })}
         </Row>
       </Container>
     </>
